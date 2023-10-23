2dc92a1f492509528dfb901818b1098a50a6edd4
juanesteban-cj7 <148505360+juanesteban-cj7@users.noreply.github.com>
2023-10-23 16:41:20 -0500

    sketch v0.0 BETA

---
+++ sketch.cpp
@@ -0,0 +1,21 @@
+void setup() {
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+  digitalWrite(9, 1);
+}
